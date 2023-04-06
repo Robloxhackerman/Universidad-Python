f274da4f3db42fa37934c389fe5a6bca264b4901
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-06 17:43:34 -0300

    task 0

---
+++ Leccion-02/0-Rectángulo.py
@@ -0,0 +1,8 @@
+alto = int(input('Ingrese el alto del rectangulo: '))
+ancho = int(input('Ingrese el acnho del rectangulo'))
+
+area = alto * ancho
+perimetro = (alto * 2) + (ancho * 2)
+
+print(f"Area: {area}")
+print(f"Perimetro: {perimetro}")
