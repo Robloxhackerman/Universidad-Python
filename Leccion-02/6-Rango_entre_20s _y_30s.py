6bc751bf141345995c7cb6dc0ffea322d085c495
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-15 17:48:25 -0300

    Ej 6

--- Leccion-02/6-Rango_entre_20s _y_30s.py
+++ Leccion-02/6-Rango_entre_20s _y_30s.py
@@ -4,6 +4,11 @@ veintes = edad >= 20 and edad < 30
 treintas = edad >= 30 and edad < 40
 
 if veintes or treintas:
-    print("Estas dentro del rango.")
+    print("Dentro del rango de los 20's o 30's")
+
+    if veintes:
+        print("Dentro del rango de los 20's")
+    elif treintas:
+        print("Dentro del rango de los 30's")
 else:
     print("No estas dentro del rango.")
