b2ce7d0ac84902d517f6013ae05acfec9706b5c6
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2023-04-15 18:05:15 -0300

    Ej 7

--- Leccion-02/7-El_mayor_de_dos_numeros.py
+++ Leccion-02/7-El_mayor_de_dos_numeros.py
@@ -1,16 +1,3 @@
-"""
-Instrucciones de tareas:
-
-Solicitar al usuario dos valores, y determinar cual número es el mayor
-
-numero1 (int)
-numero2 (int)
-Se debe imprimir el mayor de los dos números (la salida debe ser identica a la que sigue):
-Proporciona el primer valor:
-Proporciona el segundo valor:
-El numero mayor es:<numeroMayor>
-"""
-
 numero1 = int(input("Proporciona el primer valor: "))
 numero2 = int(input("Proporciona el segundo valor: "))
 
